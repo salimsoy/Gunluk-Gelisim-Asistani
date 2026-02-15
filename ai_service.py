0843f2b786fd7fc01ec2c115ac04a069c75a4f58
salim soy <salimsoy0320@gmail.com>
2026-02-15 15:43:11 +0300

    AI bağlantılarının ve yönetiminin yapıldığı servis kodlarını yükler

---
+++ ai_service.py
@@ -0,0 +1,52 @@
+import google.generativeai as genai
+import json
+
+class AIService:
+    def __init__(self, API_KEY):
+        genai.configure(api_key=API_KEY) 
+    
+    def model_selection(self):
+        # modeli oluşturur
+        model = genai.GenerativeModel('gemini-2.5-flash')
+        return model
+
+    def prompter(self, user_input):
+        #verilecek promptu oluşturur
+        prompt = f"""
+                Sen bir öğrenme analizi yapan yapay zekâ asistanısın.
+                Görevin: Kullanıcının "Bugün ne öğrendim?" metnini analiz etmek.
+                ÇIKTI KURALLARI (ÇOK KRİTİK):
+                - SADECE geçerli JSON döndür.
+                - JSON dışında TEK BİR KELİME bile yazma.
+                - Açıklama, yorum, giriş cümlesi yazma.
+                - Alan adlarını ASLA değiştirme.
+                - Etiket sayısı TAM OLARAK 3 olmalı.
+                - Cevaplar net ve Türkçe olmalı.
+                İstenen JSON formatı:
+                {{
+                    "ozet": "Öğrenilen bilginin kısa özeti",
+                    "etiketler": ["etiket1", "etiket2", "etiket3"],
+                    "yorum": "Basit bir geri bildirim yorumu",
+                    "oneri": "Kısa gelişim önerisi"
+                }}
+                Kullanıcı metni:{user_input}
+                """
+        return prompt
+    
+    def regulator(self, response):
+        # json metnini temizletip anlamlı hale getirir
+        clean_text = response.text.replace("```json", "").replace("```", "").strip()
+        res_json = json.loads(clean_text)
+        return res_json
+    
+    def main(self, user_input):
+        model = self.model_selection()
+
+        prompt = self.prompter(user_input)
+
+        response = model.generate_content(prompt)
+
+        res_json = self.regulator(response)
+
+        return res_json
+
